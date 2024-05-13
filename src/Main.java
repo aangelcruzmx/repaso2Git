93f8e31153f8f5f828768c508029bd85fb02cae4
aangelcruz <aangelcruz@gmail.com>
2024-05-13 21:53:53 +0200

    Primer commit rama usuario 1 metodo saludar

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        saludar();
 
 
+
+
+    }
+
+    private static void saludar() {
+
+        System.out.println("Hello world");
     }
 }
