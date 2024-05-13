42286883eebe1388cff2e799563422dfb523ce3c
aangelcruz <aangelcruz@gmail.com>
2024-05-13 22:01:00 +0200

    Primer commit rama usuario 2 metodo despedirse

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
         saludar();
+        despedirse();
 
 
 
 
+    }
+
+    private static void despedirse() {
+        System.out.println("hasta la vista baby");
     }
 
     private static void saludar() {
